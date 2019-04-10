f6d122517c788e02bac32c968ba3fc2c7a48c709
arantxa-e <37158609+arantxa-e@users.noreply.github.com>
2019-04-10 17:09:56 -0400

    Added project name to the title

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css">
+    <title>Print My Nonsense</title>
+  </head>
+  <body>
