ad86d76d196f6c2a832f6ab766dff6cd9dd3765e
arantxa-e <37158609+arantxa-e@users.noreply.github.com>
2019-04-20 12:09:55 -0400

    Added image editor api

--- editor.php
+++ editor.php
@@ -18,9 +18,27 @@
 
     <h2 class="title is-3 has-text-centered">Image Editor</h2>
     <!-- Image Editor Here -->
-    <p>
-      Image editor will go here.
-    </p>
+    
+    <div class="columns">
+        <div class="column has-text-centered"><h3 class="title is-4">Your Uploaded Image</h3>
+        <?php if(isset($_SESSION['image'])) {
+            echo '<a href="https://www.ribbet.com/app/?_import=http://printmynonsense.com/'.$_SESSION['image'].'&_export=http://printmynonsense.com/editor.php&_exclude=out,home,share& _export_title=Save&_export_agent=browser&embed=true"><img src="' . $_SESSION['image'] .'" class="responsive" /></a><br /><a class="button" href="https://www.ribbet.com/app/?_import=http://printmynonsense.com/'.$_SESSION['image'].'&_export=http://printmynonsense.com/editor.php&_exclude=out,home,share& _export_title=Save&_export_agent=browser&embed=true">Edit Image</a> ';
+         } else {
+             echo 'You haven\'t selected an image. Please <a href="index.php">go back</a> to upload an image.';
+         } ?>
+         </div>
+         <div class="column has-text-centered"><h3 class="title is-4">Your Edited Image</h3>
+         <?php
+         
+         if(isset($_GET['file'])) {
+             $_SESSION['edit'] = $_GET['file'];
+              echo '<img src="'. $_SESSION['edit'] .'" />';
+         } else {
+            echo 'Click on your image to open the editor. After you save, your edited image will appear here.';
+         }
+        ?>
+        </div>
+    </div>
 
 
     <div class="has-text-centered">
@@ -28,3 +46,4 @@
     </div>
 
 <?php include('includes/footer.php'); ?>
+
