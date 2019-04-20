b9d447cf76b1b71b9f3b0a462abb5ecae3910ab9
arantxa-e <37158609+arantxa-e@users.noreply.github.com>
2019-04-20 12:07:48 -0400

    Added database info

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+  $username = "ysvxqetq_pmn";
+  $password = "]*Xc@^-YYxZt";
+  $dbname = "ysvxqetq_pmn";
+
+  $connection = mysqli_connect("localhost" , "$username" , "$password", "$dbname") or die(mysql_error());
+
+?>
