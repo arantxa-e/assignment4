307e5db25577582a822137b2d3605f0d2a3eca9f
arantxa-e <37158609+arantxa-e@users.noreply.github.com>
2019-04-20 12:06:55 -0400

    File upload

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php include('includes/header.php'); ?>
+<?php include('config/db.php'); ?>
+
+<script
+  src="https://code.jquery.com/jquery-3.4.0.min.js"
+  integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg="
+  crossorigin="anonymous"></script>
+  <script src="http://malsup.github.com/jquery.form.js"></script>
 
   <section class="hero is-light">
     <div class="hero-body">
@@ -24,38 +31,72 @@
           Upload your image below.
         </p>
 
-        <form action="" method="post" class="has-text-centered">
-          <div class="field">
-            <label class="file-label">
-              <input class="file-input" type="file" name="uploadFile" id="uploadFile" accept=".jpg, .png" />
-              <span class="file-cta">
-                <span class="file-icon">
-                  <i class="fas fa-upload"></i>
-                </span>
-                <span class="file-label">
-                  Choose a file…
-                </span>
-              </span>
-            </label>
-          </div>
 
-          <div class="control">
-            <a class="button is-light" id="uploadSubmit">Upload</a>
-          </div>
 
-          <!-- Animated progress bar. Will need to be visible when image is loading
-          <progress class="progress is-small is-primary" max="100">15%</progress> -->
 
-        </form>
-      </div>
+           <form id="uploadImage" action="upload.php" method="post">
+            <div class="form-group">
+             <label>File Upload (.jpg or .png): </label>
+             <input type="file" name="uploadFile" id="uploadFile" accept=".jpg, .png" />
+            </div>
+            <div class="form-group">
+             <input type="submit" id="uploadSubmit" value="Upload" class="button" />
+            </div>
+            <div class="progress">
+             <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            </form>
 
-      <div class="column">
+       </div><!-- /column -->
+
+    <div class="column">
         <h3 class="title is-4 has-text-centered">Your Image</h3>
-      </div>
+        
+            <div id="targetLayer" style="display:none;"></div>
+
+            <div id="loader-icon" style="display:none;"><img src="img/loader.gif" /></div>
+           
     </div>
+
+  </div><!-- /columns -->
 
     <div class="has-text-centered">
       <a class="button is-primary is-medium" href="editor.php">Next</a>
     </div>
 
+
+
 <?php include('includes/footer.php'); ?>
+
+<script>
+        $(document).ready(function(){
+         $('#uploadImage').submit(function(event){
+          if($('#uploadFile').val())
+          {
+           event.preventDefault();
+           $('#loader-icon').show();
+           $('#targetLayer').hide();
+           $(this).ajaxSubmit({
+            target: '#targetLayer',
+            beforeSubmit:function(){
+             $('.progress-bar').width('0%');
+            },
+            uploadProgress: function(event, position, total, percentageComplete)
+            {
+             $('.progress-bar').animate({
+              width: percentageComplete + '%'
+             }, {
+              duration: 1000
+             });
+            },
+            success:function(){
+             $('#loader-icon').hide();
+             $('#targetLayer').show();
+            },
+            resetForm: true
+           });
+          }
+          return false;
+         });
+        });
+    </script>
